7cdc6b0ad59a52565c2353abd53745272cd182fe
Yaakoub tair <yaakoubt05@gmail.com>
2025-09-08 13:23:11 +0200

    challenge 9

---
+++ challenge_9.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main(){
+    int taille;
+    printf("donner la taille du tableau:\n");
+    scanf("%d",&taille);
+
+    int tab[taille];
+    for (int i = 0; i < taille; i++)
+    {
+       printf("saisir le %d element du tableau:\n ",i+1);
+       scanf("%d",&tab[i]);
+    }
+    int copietab[taille];
+    int j=taille-1;
+    for (int i = 0; i < taille; i++){
+        copietab[j]=tab[i];
+        j--;
+    
+    
+    }
+    printf("voici les element du tableau original :\n");
+    for (int i = 0; i < taille; i++)
+    {
+        printf("%d  ",tab[i]);
+    }
+     printf("\n voici les element inverser  du tableau  :\n");
+    for (int i = 0; i < taille; i++)
+    {
+        printf("%d  ",copietab[i]);
+    }
+
+
+    return 0;
+}
